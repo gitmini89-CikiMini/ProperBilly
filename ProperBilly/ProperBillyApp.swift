f2b3181c7f6ca082232f42cf779d0de1a58740ba
gitmini89-CikiMini <gitmini89@gmail.com>
2025-10-29 15:07:45 +0100

    Initial Commit

---
+++ ProperBilly/ProperBillyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProperBillyApp.swift
+//  ProperBilly
+//
+//  Created by Mi Ni on 29/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ProperBillyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
