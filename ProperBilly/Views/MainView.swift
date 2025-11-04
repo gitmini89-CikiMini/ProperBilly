8b3c97fa32ee0c3e001b585ce603a0d0942105bb
MiNi89iOS <gitmini89@gmail.com>
2025-11-04 12:05:28 +0100

    MainRow added

---
+++ ProperBilly/Views/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  ProperBilly
+//
+//  Created by Mi Ni on 04/11/2025.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
