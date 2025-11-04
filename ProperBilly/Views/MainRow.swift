9a53c601c30c9a032cc9efdab03bca3bacce5438
MiNi89iOS <gitmini89@gmail.com>
2025-11-04 12:41:19 +0100

    MainRow view update

--- ProperBilly/Views/MainRow.swift
+++ ProperBilly/Views/MainRow.swift
@@ -17,13 +17,13 @@ struct MainRow: View {
     let propertyColor: Color
     
     var body: some View {
-        ZStack {
+        ZStack(alignment: .leading) {
             propertyColor
-                .frame(height: 156)
+                .frame(height: 150)
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .padding()
             
-            HStack(alignment: .center, spacing: 10) {
+            HStack(spacing: -10) {
                 (propertyImage ?? Image("placeholder"))
                     .resizable()
                     .scaledToFill()
@@ -35,31 +35,34 @@ struct MainRow: View {
                             .fill(Color.white)
                     )
                     .clipShape(RoundedRectangle(cornerRadius: 6))
-                    .padding(13)
+                    .padding(10)
                 
                 VStack(alignment: .leading, spacing: 4) {
                     Text(propertyName)
-                        .font(.title3)
+                        .font(.headline)
+                        .foregroundStyle(.black)
                         .bold()
                     
                     Spacer()
                     
                     Text(propertyType)
-                        .font(.title3)
-                        .foregroundStyle(.secondary)
+                        .font(.title2)
+                        .foregroundStyle(.black)
                         .bold()
                     
                     Spacer()
                     
                     Text(propertyAddressLine_1)
                         .font(.subheadline)
+                        .foregroundStyle(.black)
                         .bold()
                     Text(propertyAddressLine_2)
                         .font(.subheadline)
+                        .foregroundStyle(.black)
                         .bold()
                 }
-                .frame(maxWidth: .infinity, maxHeight: 120, alignment: .leading)
-               // .padding()
+                .frame(maxHeight: 120, alignment: .leading)
+                .padding()
             }
             .padding()
         }
@@ -69,7 +72,7 @@ struct MainRow: View {
 #Preview {
     MainRow(
         propertyImage: Image(systemName: "house"),
-        propertyName: "M-PIA-CUD7/99",
+        propertyName: "W-WW-WW0/00",
         propertyType: "Mieszkanie",
         propertyAddressLine_1: "Cudowna 7/99",
         propertyAddressLine_2: "05-500 Piaseczno",
